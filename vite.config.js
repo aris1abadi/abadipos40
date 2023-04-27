dd233e2f0d10ab53658943fcba241ec4f5ff956e
aris1abadi <aris1abadi@gmail.com>
2023-04-27 14:51:14 +0700

    modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { sveltekitSocketIo } from './src/lib/serverSocketIO';
 
 const config= {
-	plugins: [sveltekit(), sveltekitSocketIo()]
+	//plugins: [sveltekit(), sveltekitSocketIo()]
+	plugins: [sveltekit()]
 };
 
 export default config;
